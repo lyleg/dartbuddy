a8abfc08dcfa4a12941c6208e692279d92a359a9
Lyle Garza <morezero06@gmail.com>
2015-10-07 10:01:10 -0500

    build and package update

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,4 @@
 npm run clean
-npm run copyHtml
-set -o pipefail NODE_ENV=production browserify js/index.js -t babelify -t envify > dist/js/bundle.js
+npm run copyStatic
+npm run build-scss
+NODE_ENV=production browserify js/index.js -t babelify -t envify | uglifyjs -cm -warnings=false --screw-ie8 > dist/js/bundle.js
